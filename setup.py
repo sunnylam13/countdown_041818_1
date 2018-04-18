08e7878432a74af671aa94edc84edffa03d1ab8e
Sunny Lam <sunny.lam@gmail.com>
2018-04-18 09:37:02 -0400

    revised setup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
 	'description': 'A simple countdown program that plays an alarm at the end of the countdown.',
 	'author': 'Sunny Lam',
-	'url': 'URL to get it at',
-	'download_url': 'Where to download it',
+	'url': 'https://github.com/sunnylam13/countdown_041818_1',
+	'download_url': 'https://github.com/sunnylam13/countdown_041818_1',
 	'author_email': 'sunny.lam@gmail.com',
 	'version': '0.1',
 	'install_requires': ['nose'],
